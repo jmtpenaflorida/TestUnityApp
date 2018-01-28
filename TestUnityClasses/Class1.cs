700885869cf677d56409e50d01702612b0255fd5
Jay <jmtpenaflorida@gmail.com>
2018-01-29 00:04:01 +0800

    Add project files.

---
+++ TestUnityClasses/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestUnityCommon;
+
+namespace TestUnityClasses
+{
+  
+    public class SolidUnityClass : IUnityInterface
+    {
+        public string Get()
+        {
+            return "Solid";
+        }
+    }
+
+    public class LiquidUnityClass : IUnityInterface
+    {
+        public string Get()
+        {
+            return "Liquid";
+        }
+    }
+}
---
+++ TestUnityConsoleApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Practices.Unity;
+using Microsoft.Practices.Unity.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestUnityClasses;
+using TestUnityCommon;
+
+namespace TestUnityConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UnityContainer myContainer = new UnityContainer();
+            UnityConfigurationSection section = (UnityConfigurationSection)ConfigurationManager.GetSection("unity");
+            section.Containers["containerOne"].Configure(myContainer);
+
+            Console.WriteLine(myContainer.Resolve<IUnityInterface>().Get());
+
+            Console.ReadLine();
+
+        }
+    }
+
+   
+}
